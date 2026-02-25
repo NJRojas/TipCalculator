6cb33633eb562fb165c1fc4506d980e465948037
Neyla Johanna Rojas Pena <neylarojas.developer@gmail.com>
2026-02-25 14:58:53 +0100

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by NJ Rojas on 25.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
